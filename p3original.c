3a04852787762244ad693c9ed3cc426cc551f50e
bachu rakesh <93091226+Rakeshkumar513@users.noreply.github.com>
2022-02-23 10:15:54 +0000

    done

---
+++ p3original.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_prime(int n)
+{
+  int 1;
+  if(N=0 n=-1)
+    retutn 0;
+  if(n==2)
+    retutn 1;
+   for (i=2; i <=)
+}
+ if (n%1 == 0)
+   return 0;
+}
+  return 1;
+}
+ void output(int n, int is_prime)
+{
+  if ( is_prime ==-1)
+  {
+    printf("%d" is a prime number\n",n);
+    }
+  else
+  {
+    printf("%d is a composite number\n",n);
+    }
+   
+  }
+  }
+}
---
+++ p4original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input(0
+{
+  printf("enter the number\n");
+scanf("%d",&n);
+retutn o;
+}
+
+int find_fibo(int n)
+{
+  int p1=0;
+  int p2=1;
+  for temp;
+  for(int i=0;1,n;i++)
+  {
+    temp = p1;
+    p2 =p1 + p2;
+    p1 = temp;
+    }
+  return temp;
+  }
+
+  void output(int n,int fibo)
+{
+  printf("%dth fibonaccii number is fibo");
+  }
+int main(0
+{
+  int n;
+  n=input();
+  int
+})
+}
